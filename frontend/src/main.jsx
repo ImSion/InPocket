80e45b196a0db2b949a16a8374acf851f85a4dce
ImSion <Sion931@gmail.com>
2024-09-06 12:23:02 +0200

    some fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,8 +5,14 @@ import './index.css'  // Importa lo stile CSS globale
 import { Auth0Provider } from '@auth0/auth0-react';  // Importa Auth0Provider per l'autenticazione
 
 // Ottiene le variabili d'ambiente per Auth0
-const domain = import.meta.env.VITE_AUTH0_DOMAIN  // Domain di Auth0
-const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID  // Client ID di Auth0
+const domain = import.meta.env.VITE_AUTH0_DOMAIN // Domain di Auth0
+const clientId = import.meta.env.MODE === 'development' 
+  ? import.meta.env.VITE_AUTH0_CLIENT_ID_SV
+  : import.meta.env.VITE_AUTH0_CLIENT_ID;
+
+const callbackUrl = import.meta.env.MODE === 'development' 
+  ? 'http://localhost:5173'
+  : 'https://in-pocket.vercel.app/login';
 
 // Crea e renderizza l'applicazione React
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -16,7 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       domain={domain}  // Configura il domain di Auth0
       clientId={clientId}  // Configura il client ID di Auth0
       authorizationParams={{
-        redirect_uri: window.location.origin  // Imposta l'URI di reindirizzamento dopo il login
+        redirect_uri: callbackUrl
       }}
       useRefreshTokens={true}  // Abilita l'uso dei token di refresh
       cacheLocation="localstorage"  // Memorizza i token nel localStorage
