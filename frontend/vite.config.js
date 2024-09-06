234ed824883dd33ffdb84733c3a40c0f3705ceb4
ImSion <Sion931@gmail.com>
2024-09-06 12:51:54 +0200

    fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5001',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+  }
+
+  if (command !== 'serve') {
+    // configurazione per la build di produzione
+    return config
+  }
+
+  // configurazione per lo sviluppo
+  return {
+    ...config,
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5001',
+          changeOrigin: true,
+          secure: false,
+        }
       }
     }
   }
